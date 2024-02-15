c3c11ece79a98bb48b3cbeef6ed0a0a4935c316d
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-02-14 16:55:39 -0800

    initial commit

---
+++ databaseConnection.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql2/promise');
+
+const is_render = process.env.IS_RENDER || false;
+
+const dbConfigRender = {
+	host: "sql.freedb.tech",
+	user: "freedb_cwan_main",
+	password: "n!?hTMRsJ2GNPM*",
+	database: "freedb_comp2350-week2-A00970304",
+	multipleStatements: false
+};
+
+const dbConfigLocal = {
+	host: "sql.freedb.tech",
+	user: "freedb_cwan_main",
+	password: "n!?hTMRsJ2GNPM*",
+	database: "Free DB",
+	multipleStatements: false
+};
+
+if (is_render) {
+	var database = mysql.createPool(dbConfigRender);
+}
+else {
+	var database = mysql.createPool(dbConfigLocal);
+}
+
+module.exports = database;
+		
