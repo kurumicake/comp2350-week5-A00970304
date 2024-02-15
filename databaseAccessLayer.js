f685a40abf3a49af7ffaa230ec780b7499c5d2f9
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-02-14 21:40:16 -0800

    adding user

--- databaseAccessLayer.js
+++ databaseAccessLayer.js
@@ -3,7 +3,7 @@ const database = include('/databaseConnection');
 
 async function getAllUsers() {
 	let sqlQuery = `
-		SELECT * FROM web_user
+		SELECT web_user_id, first_name, last_name, email FROM web_user
 	`;
 	
 	try {
