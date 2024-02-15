a21e48211ccac74a39837b93076be7410650f6c0
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-02-14 21:33:59 -0800

    change all to render and web user

--- databaseAccessLayer.js
+++ databaseAccessLayer.js
@@ -1,9 +1,9 @@
 const database = include('/databaseConnection');
 
 
-async function getAllTodos() {
+async function getAllUsers() {
 	let sqlQuery = `
-		SELECT * FROM todo
+		SELECT * FROM web_user
 	`;
 	
 	try {
@@ -12,11 +12,11 @@ async function getAllTodos() {
 		return results[0];
 	}
 	catch (err) {
-		console.log("Error selecting from todo table");
+		console.log("Error selecting from User table");
 		console.log(err);
 		return null;
 	}
 }
 
 
-module.exports = {getAllTodos}
+module.exports = {getAllUsers}
